ddc0cc5c92a56d3a590a226e3921ff8dfac6af6e
David Hodgson <davidhodgson@davids-mbp.home>
2019-10-10 10:22:57 +0100

    initial commit in branch

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 //  Copyright © 2019 David Hodgson. All rights reserved.
 //
 
+// Necessary includes
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -13,16 +14,20 @@
 #include <chrono>
 #include <ctime>
 
-#define PI 3.14159265
-#define Cal5
-#define DynamicMat
+// Preprocessor derivatives
+#define PI 3.14159265   // Global definition of pi
+#define Cal5            // Which detection model to use (calX for detection model X)
+#define DynamicMat      // Which maternal immunity model to use (comment out -> static immunity)
 //#define Minutes
 
 using namespace std;
 
-
+// Non-boost dependencies (in header files)
 #include <math.h>
 #include <cmath>
+#include <algorithm>
+#include <vector>
+#include <random>
 
 // Boost dependencies
 #include <boost/random.hpp>
@@ -31,26 +36,30 @@ using namespace std;
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/normal_distribution.hpp>
 
-#include <algorithm>    // std::max
-#include <vector>
-#include <random>
-
-// Prepackaged header files
+// Prepackaged header files (look in header folder)
 #include "Eigen/Dense"
 #include "ascent/Ascent.h"
 using namespace asc;
 using namespace Eigen;
 
-// Other header files
-#include "pre.h"
-#include "epmgp.h"
-#include "model.h"
-#include "mcmc.h"
-#include "interventions.h"
-#include "outcomes.h"
-#include "cea.h"
-#include "writing.h"
+// Model header files
+#include "pre.h"            // Soem preliminary stuff
+#include "epmgp.h"          // Contains code for evalauting from a truncated multivariate normal distribution
+#include "model.h"          // Description of model to be calibrated
+#include "mcmc.h"           // Outlines the mcmc methods for calibration
+#include "interventions.h"  // Intervention model code and scenarios
+#include "outcomes.h"       // Calculate the outcomes
+#include "cea.h"            // Cost-effectiveness analysis code
+#include "writing.h"        // Code for writing out the results
 
+// FUNC1:"parallel_temp_mcmc -> Run the MCMC parallel tempering algorithm
+// Arg:"paramFit" -> Parameters to calibrate
+// Arg:"Nburn" -> Burn-in
+// Arg:"Nrun" -> Total length of run
+// Arg:"Nthin_all" -> Thinning across all parameters (to view)
+// Arg:"Nthin_pos" -> Thnning after burn-in (gives posterior)
+// Arg:"Nchain" -> Number of chains used
+// Arg:"A" -> Identifying character in output
 
 void parallel_temp_mcmc(const std::vector<std::string>& paramFit, int Nburn, int Nrun, int Nthin_all, int Nthin_pos, int Nchain, char A)
 {
@@ -71,7 +80,6 @@ void parallel_temp_mcmc(const std::vector<std::string>& paramFit, int Nburn, int
     
     for (int i = 1; i < Nrun ; i++)
     {
-        ///auto wcts_2 = std::chrono::system_clock::now();
         // Swap chains adjacent chains
         mhp::update_swap(mhp_state, mcmc_state, i);
 
@@ -81,18 +89,18 @@ void parallel_temp_mcmc(const std::vector<std::string>& paramFit, int Nburn, int
         // Update the temperature ladders
         mhp::T_edit(mhp_state, mcmc_state);
         
-        //
+        // Save at thinning points
         if(i%Nthin_all==0)
             mhp::save(mhp_state, mcmc_state);
         
     }
-
     // Write data for the 4 coldest chains
     amh::write_mcmc (mcmc_state[0], pars_r[0], 'A');
     amh::write_mcmc (mcmc_state[1], pars_r[1], 'B');
     amh::write_mcmc (mcmc_state[2], pars_r[2], 'C');
     amh::write_mcmc (mcmc_state[3], pars_r[3], 'D');
 
+    // Write data on overall mcmc chains
     mhp::write_mhp_state (mhp_state, A);
 }
 
@@ -174,7 +182,6 @@ void find_optimal_week(param::param_state_t& pars, amh::amh_state_t& mcmc_state,
 void intervention_p(std::vector<std::string> paramFit, amh::amh_state_t& mcmc_state, param::param_state_t& pars,  num_vec seed, int time_hor, double disc)
 {
     // Intervention programme identifiers
-    //{{{26}}, {{26}}, {{4}}, {{46}}, {{4}}, {{13}}, {{17}}, {{21}}, {{21}}}
     // Optimal fit is XXXXX //
     VectorXd eff_pal = cal::get_eff(seed.size(), 1);
     VectorXd eff_mab =  cal::get_eff(seed.size(), 2);
@@ -211,24 +218,9 @@ void intervention_p_SA(std::vector<std::string> paramFit, amh::amh_state_t& mcmc
 
 }
 
-void test_random(int s)
-{
-    PRNG_s rng(s);
-    boost::random::weibull_distribution<> Ev(11.8975, 0.7317);
-    cout << Ev(rng) << endl;
-    cout << Ev(rng) << endl;
-}
-
 int main(int argc, const char * argv[]) {
-        
-    // arguments (vector string listing the parameters to be fitted,
-        // number of steps of burn in,
-        //  number of steps overall,
-        //  thinning of whole chain,
-       // thinning of posterior,
-        // number of parallel chains,
-        // year data is fitted to,
-        // Char for output )
+    
+    // Var:"paramFitA -> List of parameters the mcmc model is to fit. Parameters not in this list are fixed.
     std::vector<std::string> paramFitA = {"xi", "si", "g0", "g1", "g2", "om",
         "pA1", "pA2", "pA3", "pA4", "alpha_i", "d1", "d2", "d3",
         "phi", "qp", "qc", "b1",
@@ -238,12 +230,15 @@ int main(int argc, const char * argv[]) {
     };
     
 /**********************************/
-/**      CALIBRATE MODEL         **/
+/**      1. CALIBRATE MODEL         **/
 /**********************************/
+    // FUNC1:"parallel_temp_mcmc -> Run the MCMC parallel tempering algorithm
     //parallel_temp_mcmc(paramFitA, 25000, 50000, 100, 20, 12, 'A');
+    
 /**********************************/
-/**      IMPORT POSTERIOR FOR REST        **/
+/**      IMPORT POSTERIORS FOR REMAINING FUNCTIONS **/
 /**********************************/
+    
     // IMPORT POSTERIORS FROM CALIBRATION IN MCMC
     param::param_state_t pars(paramFitA);
     amh::amh_state_t mcmc_state(25000, 50000, 100, 20);
@@ -257,32 +252,38 @@ int main(int argc, const char * argv[]) {
     num_vec seed;
     for (int s = 0; s < 10; s++)
         seed.push_back(uniform_dist_disc(0, mcmc_state.NK2-1, 'r'));
-    int time_hor = 10;
-    double disc = 0.035;
+    int time_hor = 10;      // Time horizon
+    double disc = 0.035;    // Discounting
+    
 /**********************************/
-/**      EVALUATE MODEL FIT        **/
+/**      2. EVALUATE MODEL FIT        **/
 /**********************************/
     // DETERMINE INCIDENCE SAMPLES FROM POSTERIOR, R0/REFF VALUES AND INCIDENCE
+    // FUNC2:"posterior_inc -> Determine incidence from posterior samples
    // posterior_inc(paramFitA, mcmc_state, pars, seed);
    // output_fitted_dist();
     
 /**********************************/
-/** FIND OPTIMAL WEEK            **/
+/**     3. FIND OPTIMAL WEEK            **/
 /**********************************/
-    // DETERMINE THE OPTIMAL WEEEK FOR WHICH TO START THE TREATMENT
+    // FUNC3:"find_optimal_week" -> Determine the optimal month for seasonal programmes to begin
     find_optimal_week(pars, mcmc_state, seed, time_hor, disc);
     
 /**********************************/
-/** COMPARE CONSISTENCT         **/
+/**     4. COMPARE CONSISTENCY         **/
 /**********************************/
+    
+    // FUNC4a:"consistency_checks" -> XXXX
+    // FUNC4b:"write_foi_mat" -> XXXX
     
     //sim_ouput::consistency_checks(pars, mcmc_state, 1, 1.0/250.0, 1.0);
     //sim_ouput::write_foi_mat(pars, mcmc_state, 100);
     
 /**********************************/
-/**  EVALUATE INTERVENTION PROGRAMMES        **/
+/**  5. EVALUATE INTERVENTION PROGRAMMES        **/
 /**********************************/
-    // RUN THE INTERVENTION PROGRAMMES
+    // FUNC5a:"intervention_p" -> Run the simualtions for the 14 intervention progammes
+    // FUNC5b:"intervention_p_SA" -> Run the simualtions for the sensitivtiy analysis
 
     //intervention_p(paramFitA, mcmc_state, pars, seed, time_hor, disc);
     //intervention_p_SA(paramFitA, mcmc_state, pars, seed, time_hor, disc);
